e25dcf1be45e57909b7fe0e59acc827818f0a8e3
Mateo Upegui Borja <upeguiborja@gmail.com>
2021-04-29 04:42:39 -0500

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
